5a155c3cef77898c47bff23be06a8d64f2ca5a21
GreenEnvy10 <116119193+GreenEnvy10@users.noreply.github.com>
2025-05-03 09:43:19 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 const username = 'GreenEnvy10'; // your GitHub username
+const mainRepoName = `${username.toLowerCase()}.github.io`;
 const list = document.getElementById('repo-list');
 
 fetch(`https://api.github.com/users/${username}/repos`)
   .then(response => response.json())
   .then(repos => {
     repos
-      .filter(repo => repo.has_pages)
+      .filter(repo => repo.has_pages && repo.name.toLowerCase() !== mainRepoName)
       .forEach((repo, index) => {
         const li = document.createElement('li');
         li.innerHTML = `<a href="https://${username}.github.io/${repo.name}/" target="_blank">${repo.name}</a>`;
         list.appendChild(li);
 
-        // Animate each list item with a delay
         setTimeout(() => {
           li.classList.add('visible');
         }, index * 100);
